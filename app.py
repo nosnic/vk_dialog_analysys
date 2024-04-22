aa0e99ecda534645a154cde1e12ca3e9e8df4d48
nosnic <nosilkin.n.d@gmail.com>
2024-04-23 02:24:38 +0300

    add streamlit MVP only with int outputs

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ def main():
     flag = False
     flag1 = False
     flag2 = False
+    flag3 = False
     friend_names = []
     friend_dict = {}
 
@@ -34,7 +35,29 @@ def main():
 
     if flag2:
         anal.download_vk_dialog(friend_dict[selected_option])
-        st.write(anal.dialog)
+        anal.create_dataframe()
+        flag3 = True
+
+    if flag3:
+        st.write("Количество сообщений в чате", anal.get_messages_amount())
+        st.write("Количество сообщений от кажого участника:")
+        st.write( anal.get_messages_amount_split())
+        st.write('Дата первого сообщения', anal.get_date_of_first_message())
+        st.write('Количество дней общения в каждом году:')
+        st.write(anal.get_amount_of_talking_days_yearly())
+        st.write('Среднее количество слов в сообщении по диалогу', anal.get_word_count_mean())
+        st.write('Среднее количество слов в сообщении по каждому участнику:')
+        st.write(anal.get_word_count_mean_by_person())
+        st.write('Среднее количество слов в сообщении по каждому участнику:')
+        offset = st.slider(
+            label='Выбери сколько часов после сообщения должно пройти чтобы это считалось новым диалогом',
+            min_value=1,
+            max_value=40,
+            step=1)
+        st.write('Количество начатых бесед каждым участником:')
+        st.write(anal.get_amount_of_started_conversations(offset))
+
+
 
 
 if __name__ == "__main__":
